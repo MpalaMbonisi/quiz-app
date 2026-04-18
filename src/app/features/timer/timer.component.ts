6e5a93ffe9110b87bdbb3653e8c3eb9c9c20ffdd
Mbonisi Mpala <mpalambonisi63@gmail.com>
2026-04-18 03:33:40 +0200

    refactor(timer-component): simplify timer component and remove state tracking
    - remove 'warning' level logic and associated CSS classes from template and styles
    - delete the progress bar element to provide a cleaner, text-only countdown interface
    - remove 'questionTimeSubscription' and related cleanup logic from 'timer.component.ts'
    - streamline the component to focus strictly on displaying the remaining time value

--- src/app/features/timer/timer.component.ts
+++ src/app/features/timer/timer.component.ts
@@ -11,16 +11,11 @@ import { Subscription } from 'rxjs';
 })
 export class TimerComponent implements OnInit, OnDestroy {
   protected readonly timeRemaining = signal<number>(0);
-  protected readonly questionTime = signal<number>(0);
-  protected readonly warningLevel = computed(() =>
-    this.timerService.formatTime(this.timeRemaining())
-  );
   protected readonly formattedTime = computed(() => {
     return this.timerService.formatTime(this.timeRemaining());
   });
 
   private timeRemainingSubscription?: Subscription;
-  private questionTimeSubscription?: Subscription;
 
   private timerService: TimerService = inject(TimerService);
 
@@ -29,14 +24,9 @@ export class TimerComponent implements OnInit, OnDestroy {
     this.timeRemainingSubscription = this.timerService.timeRemaining$.subscribe(time => {
       this.timeRemaining.set(time);
     });
-
-    this.questionTimeSubscription = this.timerService.questionTime$.subscribe(time => {
-      this.questionTime.set(time);
-    });
   }
 
   ngOnDestroy(): void {
     this.timeRemainingSubscription?.unsubscribe();
-    this.questionTimeSubscription?.unsubscribe();
   }
 }
